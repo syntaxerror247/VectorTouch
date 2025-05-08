01012a3ea1196d381d14f0316dd138f164cdfbab
Anish <mishragames@gmail.com>
2025-05-08 14:49:39 +0530

    Layouting support (#20)

--- src/ui_widgets/zoom_menu.gd
+++ src/ui_widgets/zoom_menu.gd
@@ -6,9 +6,9 @@ const MAX_ZOOM = 512.0
 signal zoom_changed(zoom_level: float, offset: Vector2)
 signal zoom_reset_pressed
 
-@onready var zoom_out_button: Button = $ZoomOut
-@onready var zoom_in_button: Button = $ZoomIn
-@onready var zoom_reset_button: Button = $ZoomReset
+@onready var zoom_out_button: BetterButton = $ZoomOut
+@onready var zoom_in_button: BetterButton = $ZoomIn
+@onready var zoom_reset_button: BetterButton = $ZoomReset
 
 var _zoom_level: float
 
@@ -50,23 +50,24 @@ func zoom_reset() -> void:
 
 
 func update_buttons_appearance() -> void:
-	pass
-	#if _zoom_level < 0.1:
-		#zoom_reset_button.text = Utils.num_simple(_zoom_level * 100, 2) + "%"
-	#elif _zoom_level < 10.0:
-		#zoom_reset_button.text = Utils.num_simple(_zoom_level * 100, 1) + "%"
-	#elif _zoom_level < 100.0:
-		#zoom_reset_button.text = String.num_uint64(roundi(_zoom_level * 100)) + "%"
-	#else:
-		#zoom_reset_button.text = Utils.num_simple(_zoom_level, 1) + "x"
-	#
-	#var is_max_zoom := _zoom_level > MAX_ZOOM or is_equal_approx(_zoom_level, MAX_ZOOM)
-	#var is_min_zoom := _zoom_level < MIN_ZOOM or is_equal_approx(_zoom_level, MIN_ZOOM)
-	#
-	#zoom_in_button.disabled = is_max_zoom
-	#zoom_in_button.mouse_default_cursor_shape = Control.CURSOR_ARROW if\
-			#is_max_zoom else Control.CURSOR_POINTING_HAND
-	#
-	#zoom_out_button.disabled = is_min_zoom
-	#zoom_out_button.mouse_default_cursor_shape = Control.CURSOR_ARROW if\
-			#is_min_zoom else Control.CURSOR_POINTING_HAND
+	if not zoom_reset_button:
+		return
+	if _zoom_level < 0.1:
+		zoom_reset_button.text = Utils.num_simple(_zoom_level * 100, 2) + "%"
+	elif _zoom_level < 10.0:
+		zoom_reset_button.text = Utils.num_simple(_zoom_level * 100, 1) + "%"
+	elif _zoom_level < 100.0:
+		zoom_reset_button.text = String.num_uint64(roundi(_zoom_level * 100)) + "%"
+	else:
+		zoom_reset_button.text = Utils.num_simple(_zoom_level, 1) + "x"
+	
+	var is_max_zoom := _zoom_level > MAX_ZOOM or is_equal_approx(_zoom_level, MAX_ZOOM)
+	var is_min_zoom := _zoom_level < MIN_ZOOM or is_equal_approx(_zoom_level, MIN_ZOOM)
+	
+	zoom_in_button.disabled = is_max_zoom
+	zoom_in_button.mouse_default_cursor_shape = Control.CURSOR_ARROW if\
+			is_max_zoom else Control.CURSOR_POINTING_HAND
+	
+	zoom_out_button.disabled = is_min_zoom
+	zoom_out_button.mouse_default_cursor_shape = Control.CURSOR_ARROW if\
+			is_min_zoom else Control.CURSOR_POINTING_HAND
