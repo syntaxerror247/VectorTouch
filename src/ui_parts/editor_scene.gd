79a26611743636ac00d75719a6a8fab87904dcdd
Anish Mishra <mishragames@gmail.com>
2025-05-08 11:44:07 +0530

    Layouting support

--- src/ui_parts/editor_scene.gd
+++ src/ui_parts/editor_scene.gd
@@ -1,1 +1,126 @@
 extends VBoxContainer
+
+const GlobalActionsScene = preload("res://src/ui_parts/global_actions.tscn")
+const CodeEditorScene = preload("res://src/ui_parts/code_editor.tscn")
+const InspectorScene = preload("res://src/ui_parts/inspector.tscn")
+const ViewportScene = preload("res://src/ui_parts/display.tscn")
+
+@onready var panel_container: PanelContainer = $PanelContainer
+
+func _ready() -> void:
+	Configs.theme_changed.connect(update_theme)
+	Configs.layout_changed.connect(update_layout)
+	update_layout()
+	update_theme()
+
+func update_theme() -> void:
+	var stylebox := StyleBoxFlat.new()
+	stylebox.bg_color = ThemeUtils.overlay_panel_inner_color
+	stylebox.set_content_margin_all(0)
+	panel_container.add_theme_stylebox_override("panel", stylebox)
+
+
+func update_layout() -> void:
+	for child in panel_container.get_children():
+		child.queue_free()
+	
+	var top_left := Configs.savedata.get_layout_parts(SaveData.LayoutLocation.TOP_LEFT)
+	var bottom_left := Configs.savedata.get_layout_parts(SaveData.LayoutLocation.BOTTOM_LEFT)
+	
+	# Set up the horizontal splitter.
+	var main_splitter := VSplitContainer.new()
+	main_splitter.size_flags_horizontal = Control.SIZE_FILL
+	main_splitter.add_theme_constant_override("separation", 6)
+	main_splitter.split_offset = Configs.savedata.main_splitter_offset
+	main_splitter.dragged.connect(_on_main_splitter_dragged)
+	panel_container.add_child(main_splitter)
+	
+	var top_margin_container := MarginContainer.new()
+	top_margin_container.custom_minimum_size.x = 350
+	top_margin_container.begin_bulk_theme_override()
+	top_margin_container.add_theme_constant_override("margin_top", 6)
+	top_margin_container.add_theme_constant_override("margin_bottom", 6)
+	top_margin_container.add_theme_constant_override("margin_left", 6)
+	top_margin_container.end_bulk_theme_override()
+	main_splitter.add_child(top_margin_container)
+	
+	var bottom_margin_container := MarginContainer.new()
+	bottom_margin_container.add_theme_constant_override("margin_top", 6)
+	bottom_margin_container.add_child(create_layout_node(Utils.LayoutPart.VIEWPORT))
+	main_splitter.add_child(bottom_margin_container)
+	
+	var left_vbox := VBoxContainer.new()
+	left_vbox.add_theme_constant_override("separation", 6)
+	top_margin_container.add_child(left_vbox)
+	
+	var global_actions := GlobalActionsScene.instantiate()
+	left_vbox.add_child(global_actions)
+	
+	if not top_left.is_empty() and not bottom_left.is_empty():
+		# Layout parts both on top and on the bottom.
+		var top_vertical_split_container := VSplitContainer.new()
+		top_vertical_split_container.size_flags_vertical = Control.SIZE_EXPAND_FILL
+		top_vertical_split_container.add_theme_constant_override("separation", 10)
+		top_vertical_split_container.split_offset = Configs.savedata.top_vertical_splitter_offset
+		top_vertical_split_container.dragged.connect(_on_top_vertical_splitter_dragged)
+		top_vertical_split_container.add_child(create_layout_node(top_left[0]))
+		top_vertical_split_container.add_child(create_layout_node(bottom_left[0]))
+		left_vbox.add_child(top_vertical_split_container)
+	elif top_left.size() == 2 or bottom_left.size() == 2:
+		# Tabs for the different layout parts.
+		var layout_parts := top_left if bottom_left.is_empty() else bottom_left
+		var vbox := VBoxContainer.new()
+		vbox.size_flags_vertical = Control.SIZE_EXPAND_FILL
+		var buttons_hbox := HBoxContainer.new()
+		buttons_hbox.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+		buttons_hbox.add_theme_constant_override("separation", 10)
+		vbox.add_child(buttons_hbox)
+		
+		var layout_nodes: Dictionary[Utils.LayoutPart, Node] = {}
+		for part in layout_parts:
+			var layout_node := create_layout_node(part)
+			layout_nodes[part] = layout_node
+			layout_node.hide()
+			vbox.add_child(layout_node)
+		
+		var btn_group := ButtonGroup.new()
+		for i in layout_parts.size():
+			var part := layout_parts[i]
+			var btn := Button.new()
+			btn.toggle_mode = true
+			btn.text = TranslationUtils.get_layout_part_name(part)
+			btn.icon = Utils.get_layout_part_icon(part)
+			btn.theme_type_variation = "FlatButton"
+			btn.focus_mode = Control.FOCUS_NONE
+			btn.action_mode = BaseButton.ACTION_MODE_BUTTON_PRESS
+			btn.button_group = btn_group
+			for node_part in layout_nodes:
+				if node_part == part:
+					btn.pressed.connect(layout_nodes[node_part].show)
+				else:
+					btn.pressed.connect(layout_nodes[node_part].hide)
+			buttons_hbox.add_child(btn)
+			if i == 0:
+				btn.button_pressed = true
+				layout_nodes[part].show()
+		left_vbox.add_child(vbox)
+	else:
+		# Layout parts disabled.
+		if not top_left.is_empty():
+			left_vbox.add_child(create_layout_node(top_left[0]))
+		elif not bottom_left.is_empty():
+			left_vbox.add_child(create_layout_node(bottom_left[0]))
+
+func _on_main_splitter_dragged(offset: int) -> void:
+	Configs.savedata.main_splitter_offset = offset
+
+func _on_top_vertical_splitter_dragged(offset: int) -> void:
+	Configs.savedata.top_vertical_splitter_offset = offset
+
+
+func create_layout_node(layout_part: Utils.LayoutPart) -> Node:
+	match layout_part:
+		Utils.LayoutPart.CODE_EDITOR: return CodeEditorScene.instantiate()
+		Utils.LayoutPart.INSPECTOR: return InspectorScene.instantiate()
+		Utils.LayoutPart.VIEWPORT: return ViewportScene.instantiate()
+		_: return Control.new()
