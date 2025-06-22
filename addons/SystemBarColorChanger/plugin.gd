eeefeb1b86d63c01a1c1419f39b2b8f6330d3841
Anish Mishra <mishragames@gmail.com>
2025-06-22 14:21:01 +0530

    Switch to Immersive mode and remove SystemBarColorChanger plugin

--- addons/SystemBarColorChanger/plugin.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree() -> void:
-	add_autoload_singleton("SystemBarColorChanger", "res://addons/SystemBarColorChanger/SystemBarColorChanger.gd")
-
-func _exit_tree() -> void:
-	remove_autoload_singleton("SystemBarColorChanger")
